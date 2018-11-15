b206302f9192d74522af7a654ddf38e6b840cbf2
sojurner <pykim0591@gmail.com>
2018-11-15 00:03:14 -0700

    Creeate AutoSuggest file for suggestions

---
+++ src/helpers/autoSuggest.js
@@ -0,0 +1,18 @@
+import movies from './moviesTitle.json';
+
+export const getSuggestions = (value, name) => {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  if (escapedValue === '') {
+    return [];
+  }
+  const regex = new RegExp('\\b' + escapedValue, 'i');
+  return movies.filter(club => regex.test(getSuggestionValue(club, name)));
+};
+
+const escapeRegexCharacters = str => {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+};
+
+const getSuggestionValue = (suggestion, name) => {
+  return `${suggestion}`;
+};
