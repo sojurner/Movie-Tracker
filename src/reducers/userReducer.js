00eb16ff1923a9e4b0d11d8c507804decf8c51fb
sojurner <pykim0591@gmail.com>
2018-12-03 14:47:25 -0800

    Add viewReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,6 +2,17 @@ export const userReducer = (state = null, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
       return action.user;
+
+    default:
+      return state;
+  }
+};
+
+export const viewReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_VIEW':
+      return action.view;
+
     default:
       return state;
   }
