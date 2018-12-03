cf74593b6b4356f8c4752a224d0dc5dc429d8860
sojurner <pykim0591@gmail.com>
2018-12-03 14:46:25 -0800

    Edit release date to moment format

--- src/helpers/dataCleaners.js
+++ src/helpers/dataCleaners.js
@@ -1,10 +1,12 @@
+import * as moment from 'moment';
+
 export const movieCleaner = data => {
   const { results } = data;
   const modifiedObj = results.map(result => {
     return {
       movie_id: result.id,
       title: result.title,
-      release_date: result.release_date,
+      release_date: moment(result.release_date).format('MMM D, YYYY'),
       overview: result.overview,
       vote_average: result.vote_average,
       backdrop_path: `http://image.tmdb.org/t/p/original${
