c936717fb7cc31ed259afc8f4a4ad3e50862127e
sojurner <pykim0591@gmail.com>
2018-12-03 14:46:54 -0800

    Add currentView to store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import { movieReducer } from './movieReducer';
-import { userReducer } from './userReducer';
+import { userReducer, viewReducer } from './userReducer';
 import { errorReducer } from './errorReducer';
 
 const rootReducer = combineReducers({
   movies: movieReducer,
   currentUser: userReducer,
+  currentView: viewReducer,
   errors: errorReducer
 });
 
