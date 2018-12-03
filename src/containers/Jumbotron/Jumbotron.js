fa9a8230c0bf2f7ac2603ce855437ee5d62f986b
sojurner <pykim0591@gmail.com>
2018-12-03 14:41:55 -0800

    Add trailer and displayModal to state

--- src/containers/Jumbotron/Jumbotron.js
+++ src/containers/Jumbotron/Jumbotron.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getMovieTrailer } from '../../helpers/apiCalls';
 
+import { TrailerModal } from '../../components/TrailerModal/TrailerModal';
 import Slide from '../../components/Slide/Slide';
 
 import './Jumbotron.css';
@@ -10,7 +12,9 @@ export class Jumbotron extends Component {
   constructor() {
     super();
     this.state = {
-      currentIndex: 0
+      currentIndex: 0,
+      trailer: '',
+      displayModal: false
     };
   }
 
@@ -23,7 +27,7 @@ export class Jumbotron extends Component {
   };
 
   goToNextSlide = () => {
-    const jumboMovies = this.props.nowPlayingMovies.slice(0, 7);
+    const jumboMovies = this.props.nowPlayingMovies.slice(0, 20);
     if (this.state.currentIndex < jumboMovies.length - 1) {
       this.setState(prevState => ({
         currentIndex: prevState.currentIndex + 1
@@ -31,17 +35,40 @@ export class Jumbotron extends Component {
     }
   };
 
+  handleTrailerClick = async movieId => {
+    const trailerLink = await getMovieTrailer(movieId);
+    const trailer = `https://www.youtube.com/embed/${
+      trailerLink.results[0].key
+    }?autoplay=1`;
+
+    this.setState({ trailer, displayModal: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ displayModal: false });
+  };
+
   render() {
     const { currentIndex } = this.state;
-    const { nowPlayingMovies } = this.props;
-    const jumboMovies = nowPlayingMovies.slice(0, 7);
+    const { nowPlayingMovies, currentView } = this.props;
+    const jumboMovies = nowPlayingMovies.slice(0, 20);
 
     return (
-      <div className="jumbotron">
+      <div className={!currentView ? `jumbotron` : `jumbotron-half`}>
         {jumboMovies[currentIndex] && (
-          <Slide movie={jumboMovies[currentIndex]} />
+          <Slide
+            currentView={currentView}
+            handleTrailerClick={this.handleTrailerClick}
+            movie={jumboMovies[currentIndex]}
+          />
         )}
-        <section className="arrow-icons-container">
+        <section
+          className={
+            !currentView
+              ? `arrow-icons-container`
+              : `arrows-icons-container-half`
+          }
+        >
           <div className="backArrow">
             <i
               className="fas fa-angle-left jumbo-arrow"
@@ -55,6 +82,11 @@ export class Jumbotron extends Component {
             />
           </div>
         </section>
+        <TrailerModal
+          trailer={this.state.trailer}
+          displayModal={this.state.displayModal}
+          onCloseModal={this.onCloseModal}
+        />
       </div>
     );
   }
@@ -66,7 +98,8 @@ Jumbotron.propTypes = {
 
 const mapStateToProps = state => ({
   nowPlayingMovies: state.movies.nowPlaying,
-  error: state.errors.favoriteError
+  error: state.errors.favoriteError,
+  currentView: state.currentView
 });
 
 export default connect(mapStateToProps)(Jumbotron);
