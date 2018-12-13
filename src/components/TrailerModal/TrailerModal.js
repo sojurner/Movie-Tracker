6b23db7bb9a40a1915f62d467e7b58c5e17b8298
sojurner <pykim0591@gmail.com>
2018-12-13 12:49:14 -0700

    Add error handling for films with no trailers

--- src/components/TrailerModal/TrailerModal.js
+++ src/components/TrailerModal/TrailerModal.js
@@ -3,16 +3,29 @@ import Modal from 'react-responsive-modal';
 
 import './TrailerModal.css';
 
-export const TrailerModal = ({ displayModal, trailer, onCloseModal }) => {
+export const TrailerModal = ({
+  displayModal,
+  trailer,
+  onCloseModal,
+  error
+}) => {
   return (
     <Modal open={displayModal} center onClose={onCloseModal}>
-      <iframe
-        className="modal-trailer"
-        title="modal-trailer"
-        src={trailer}
-        width="1200"
-        height="800"
-      />
+      {trailer && (
+        <iframe
+          className="modal-trailer"
+          title="modal-trailer"
+          src={trailer}
+          width="1200"
+          height="800"
+        />
+      )}
+      {error && (
+        <div className="error-modal">
+          <h1 className="error-msg">{error}</h1>
+          <i className="far fa-frown" />
+        </div>
+      )}
     </Modal>
   );
 };
