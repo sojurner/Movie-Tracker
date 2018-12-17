eb759f6b24af836266b8a3566c4a3bd127f117bb
sojurner <pykim0591@gmail.com>
2018-12-16 17:48:43 -0700

    Create cleaner method for similar movies

--- src/helpers/dataCleaners.js
+++ src/helpers/dataCleaners.js
@@ -18,3 +18,21 @@ export const movieCleaner = data => {
   });
   return modifiedObj;
 };
+
+export const similarMovieCleaner = data => {
+  const modifiedObj = data.map(result => {
+    return {
+      movie_id: result.id,
+      title: result.title,
+      release_date: moment(result.release_date).format('MMM D, YYYY'),
+      overview: result.overview,
+      vote_average: result.vote_average,
+      backdrop_path: `http://image.tmdb.org/t/p/original${
+        result.backdrop_path
+      }`,
+      poster_path: `http://image.tmdb.org/t/p/original${result.poster_path}`,
+      favorite: false
+    };
+  });
+  return modifiedObj;
+};
