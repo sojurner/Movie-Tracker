1fdf547a5f93d61327df04b77ba5ac46d6c02008
sojurner <pykim0591@gmail.com>
2019-01-02 10:37:41 -0700

    configure redux logger

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
-import { fetchMovies, applyMovieSearch } from './sagas/sagas';
+import { fetchMovies } from './sagas/sagas';
 
 import rootReducer from './reducers';
 
@@ -13,11 +13,12 @@ import './index.css';
 import App from './containers/App/App';
 
 const sagaMiddleware = createSagaMiddleware();
+let middleware;
+process.env.NODE_ENV !== 'production'
+  ? (middleware = composeWithDevTools(applyMiddleware(sagaMiddleware, logger)))
+  : (middleware = applyMiddleware(sagaMiddleware));
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware, logger))
-);
+const store = createStore(rootReducer, middleware);
 
 sagaMiddleware.run(fetchMovies);
 
