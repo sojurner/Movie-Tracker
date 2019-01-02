b5092dac380736b174d88a438b09df64bacd59eb
sojurner <pykim0591@gmail.com>
2019-01-02 10:13:28 -0700

    Configure redux saga middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import { fetchMovies, applyMovieSearch } from './sagas/sagas';
 
 import rootReducer from './reducers';
 
 import './index.css';
 import App from './containers/App/App';
-import registerServiceWorker from './registerServiceWorker';
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, devTools);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware, logger))
+);
+
+sagaMiddleware.run(fetchMovies);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,4 +27,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
